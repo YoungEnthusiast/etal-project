8934f084bbfd1dcb54632926f87e1c3fa5714db3
Yus <yustaoab@gmail.com>
2022-09-13 15:27:43 +0100

    l

--- etal/settings.py
+++ etal/settings.py
@@ -68,15 +68,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
-
 
 SITE_ID = 1
 MIDDLEWARE = [
