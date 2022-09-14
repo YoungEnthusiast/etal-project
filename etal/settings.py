8a63bcc4bf4635bdad892331deac8e26cf888248
Yus <yustaoab@gmail.com>
2022-09-14 17:07:37 +0100

    l

--- etal/settings.py
+++ etal/settings.py
@@ -155,10 +155,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'etal/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'etal/static')]
 
 # djangotemplates/djangotemplates/settings.py
 
@@ -168,23 +168,15 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 #
 # # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
