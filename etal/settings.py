70abfa0ba3d95ef57544f3cf427350e7f86cdfe5
Yus <yustaoab@gmail.com>
2022-09-15 22:00:55 +0100

    l

--- etal/settings.py
+++ etal/settings.py
@@ -155,9 +155,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'home/yustaoab/pyapps/etal-project/static')
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'etal/static')]
 
 # STATICFILES_DIRS = [
