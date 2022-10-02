9ddf75fa1013ba3e31c723d0069c0c003671fd63
Yus <yustaoab@gmail.com>
2022-10-02 14:16:56 +0100

    l

--- etal/asgi.py
+++ etal/asgi.py
@@ -1,5 +1,5 @@
 import os
-
+from decouple import config
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
