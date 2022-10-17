add0230164f0c31c3438857329bd84f2d8e2cdf0
Yusuf <yustaoab@gmail.com>
2022-10-17 10:31:37 +0100

    l

--- etal/settings.py
+++ etal/settings.py
@@ -15,27 +15,6 @@ import sys
 from pathlib import Path
 from decouple import config
 
-# import zoneinfo
-#
-# from django.utils import timezone
-#
-# class TimezoneMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#
-#     def __call__(self, request):
-#         try:
-#             # get django_timezone from cookie
-#             tzname = request.COOKIES.get("django_timezone")
-#             if tzname:
-#                 timezone.activate(zoneinfo.ZoneInfo(tzname))
-#             else:
-#                 timezone.deactivate()
-#         except Exception as e:
-#             timezone.deactivate()
-#
-#         return self.get_response(request)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
