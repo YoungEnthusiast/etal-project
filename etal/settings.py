cb6c707ded6c3c7040e3adfaf5c2ad86f12f700f
Yusuf <yustaoab@gmail.com>
2022-10-17 10:35:55 +0100

    l

--- etal/settings.py
+++ etal/settings.py
@@ -107,8 +107,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'account.middleware.TimezoneMiddleware',
-
 
 ]
 
